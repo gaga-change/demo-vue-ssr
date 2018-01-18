23bdde2b862ca383a6d5ea860bcc024ec2d4e365
严俊东 <1172482914@qq.com>
2018-01-18 22:11:15 +0800

    'template'

---
+++ a3.vue.template.js
@@ -0,0 +1,34 @@
+const Vue = require('vue')
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const createRenderer = require('vue-server-renderer').createRenderer
+const renderer = createRenderer({
+	template: fs.readFileSync('./index.template.html', 'utf-8')
+})
+
+const context = {
+	title: 'vue-ssr'
+}
+
+app.get('*', (req, res) => {
+	const app = new Vue({
+		template: `<div> <div>{{name}}</div> <div>url: {{url}}</div> </div>`,
+		data: {
+			name: 'gaga',
+			url: req.url
+		}
+	})
+	renderer.renderToString(app, context, (err, html) => {
+		if (err) {
+			console.log(err)
+			res.send(404)
+		} else {
+			res.send(html)
+		}
+	})
+})
+
+app.listen('3000', () => {
+	console.log('listen to 3000')
+})
